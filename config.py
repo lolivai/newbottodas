1646d81556b65abbfdd9eb6b22030d30ddebf4cd
lolivai <103974102+lolivai@users.noreply.github.com>
2022-12-13 22:46:10 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+# Bot
+BOT_TOKEN = '5444581119:AAFTxQF9x0RXHsMA58rXCpCrAYQIrTEDl3A'
+TG_API_ID = '13233271'
+TG_API_HASH = 'e3ce8145aa657c2a4cc5cf0f7183e476'
+TG_ADMIN = 'Ghosthell0210'
+TG_USER = 'az9az999999'
+# Database
+DB_LOCAL = False
+DB_HOST = 'db4free.net'
+DB_HOST_USERNAME = 'ghosthell'
+DB_HOST_PASSWORD = 'Ghost0210'
+DB_NAME = ''
+
+if DB_LOCAL:
+    # Database Local
+    DB_HOST = ''
+    DB_HOST_USERNAME = 'root'
+    DB_HOST_PASSWORD = ''
+    DB_NAME = 'clutilprodb'
